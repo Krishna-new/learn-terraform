a2152be82102b0eff7c8fb4836b4e9c4474c4bec
krishna-new <donthikrishnapriya@gmail.com>
2023-04-26 19:24:53 +0530

    updated

--- sample.tf
+++ sample.tf
@@ -1,7 +1,8 @@
-resource "aws_instance" "ami"{
-  ami="ami-0b5a2b5b8f2be4ec2"
-  instance-type="t3.micro"
-}
-tags = {
-  Name="Hello world"
+resource "aws_instance" "ami" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance-type = "t3.micro"
+
+  tags = {
+    Name = "Hello world"
+  }
 }
