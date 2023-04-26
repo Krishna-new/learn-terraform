1e3adb6775abb46b01f60de5bae96f5bab7e9faa
krishna-new <donthikrishnapriya@gmail.com>
2023-04-26 19:20:49 +0530

    terraform code

--- sample.tf
+++ sample.tf
@@ -1,6 +1,4 @@
-variable sample_variable{
-  default="Hello world!"
-}
-output sample_variable{
-  value="variable.sample_variable"
+resource "aws_instance" "ami"{
+  ami="ami-0b5a2b5b8f2be4ec2"
+  instance-type="t3.micro"
 }
