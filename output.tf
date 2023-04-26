795935171d3289189a3323f1e60830845680e986
krishna-new <donthikrishnapriya@gmail.com>
2023-04-26 20:51:33 +0530

    updated variables

---
+++ output.tf
@@ -0,0 +1,30 @@
+output "sample1"{
+  value=var.sample1
+}
+#to print the lists
+output "sample2"{
+  value= var.sample2[1]
+}
+
+#to print the dict values
+output "sample_dict"{
+  value=var.sample_dict["number"]
+}
+output "sample_dict2"{
+  value=var.sample_dict["string"]
+}
+
+#to print the env variavble
+output env{
+  value=var.env
+}
+
+#to print auto-tfvars
+output auto_ex1{
+  value=var.autoex1
+}
+#to print the terraform.tfvars
+output sample_new{
+  value=var.sample_new
+}
+
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "sample1"{
+  default="Hello world"
+}
+#to provide values in the list format
+variable "sample2"{
+  default =[
+    100,
+    "Hello World"
+
+]
+}
+
+#dictionary
+variable "sample_dict"{
+  default={
+    number=100
+    string="Hello world"
+    boolean="true"
+  }
+}
+#command terraform apply -var-file=dev.tfvars--> to provide the env dynamically
+variable env{
+
+}
+#to provide the auto variable
+#command terraform apply -var-file=dev.tfvars -auto-approve--> to provide the env dynamically
+variable autoex1{
+}
+#to provide the terraform.tfvars
+variable sample_new{}
